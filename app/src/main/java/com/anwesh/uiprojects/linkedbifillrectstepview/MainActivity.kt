788d3ce6ac47327e230f658c9c9e78baae9c358d
Anwesh43 <anweshthecool0@gmail.com>
2018-10-12 10:19:31 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbifillrectstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbifillrectstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbifillrectstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.bifillrectstepview.BiFillRectStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiFillRectStepView.create(this)
     }
 }
