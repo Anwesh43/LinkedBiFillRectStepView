2d3fbf470ee126503d7984938df155d1467fab6a
Anwesh43 <anweshthecool0@gmail.com>
2018-10-12 10:09:53 +0530

    adding static create method

--- bifillrectstepview/src/main/java/com/anwesh/uiprojects/bifillrectstepview/BiFillRectStepView.kt
+++ bifillrectstepview/src/main/java/com/anwesh/uiprojects/bifillrectstepview/BiFillRectStepView.kt
@@ -4,6 +4,7 @@ package com.anwesh.uiprojects.bifillrectstepview
  * Created by anweshmishra on 12/10/18.
  */
 
+import android.app.Activity
 import android.view.View
 import android.view.MotionEvent
 import android.graphics.Canvas
@@ -194,6 +195,15 @@ class BiFillRectStepView(ctx : Context) : View(ctx) {
             bfrs.startUpdating {
                 animator.start()
             }
+        }
+    }
+
+    companion object {
+
+        fun create(activity : Activity) : BiFillRectStepView {
+            val view : BiFillRectStepView = BiFillRectStepView(activity)
+            activity.setContentView(view)
+            return view
         }
     }
 }
